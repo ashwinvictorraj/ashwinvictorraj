31725f5b456ffcb9d44b00d1c176db7319af9a5d
victor <ashwinvictorraj@gmail.com>
2022-01-10 09:27:36 +0530

    task finished

---
+++ src/org/git/Company.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class Company {
+	
+	private void employeeName() {
+		System.out.println("Ashwin");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Company c = new Company();
+		c.employeeName();
+		
+	}
+
+}
